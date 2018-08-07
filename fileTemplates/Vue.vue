3923e5d8d83d660a31a0a5944fd492b9aabbf0a4
zhenglikun <m13932952737@163.com>
2018-08-07 10:09:36 +0800

    WS-2017.2.3 <DELL@DESKTOP-CRO0CV9 Create baseRefactoring.xml, IntelliLang.xml, ide.general.xml, git.xml, _@user_Default.icls, CodeGlance.xml, project.default.xml, Copy of Less File.less, Default copy.xml, other.xml, Default.xml, Json.json, applicationLibraries.xml, translation.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, Unnamed.jade, notifications.xml, github_settings.xml, Default.xml, Vue.vue, Unnamed (1).txt, Xtpl.xtpl, php.php, JavaScript.xml, laf.xml, storage_data.xml, path.macros.xml, vcs.xml, editor.codeinsight.xml, editor.xml, debugger.xml

---
+++ fileTemplates/Vue.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+		components: {},
+		data(){
+		return{
+		    }
+		  },
+		  methods:{
+		  },
+		  mounted(){}
+		}
+</script>
+<style lang='less'>
+</style>
